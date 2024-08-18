6e757f02453d468b49dc3532c4e5818dbd5d6571
Mumina033 <mumina.mohamed033@gmail.com>
2024-08-18 02:02:16 -0400

    implemented a search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Yer
+      <div className="container">
+        <header className="App-header">
+          <h1 className="Header">Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Mumina Mohamed</small>
+        </footer>
+      </div>
     </div>
   );
 }
