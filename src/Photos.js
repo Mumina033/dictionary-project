38333683083f41fcecbd1b56405b58e20fbd1148
Mumina033 <mumina.mohamed033@gmail.com>
2024-08-29 16:05:54 -0400

    added more css to the image

--- src/Photos.js
+++ src/Photos.js
@@ -12,7 +12,7 @@ export default function Photos(props) {
                 <a
                   href={photo.src.original}
                   target="_blank"
-                  rel="noopener noreferrer"
+                  rel=" noopener noreferrer"
                 >
                   <img src={photo.src.landscape} className="img-fluid" alt="" />
                 </a>
